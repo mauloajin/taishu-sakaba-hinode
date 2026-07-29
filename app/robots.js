435f58ac59464501ce7a448f7849d561fe893895
mauloajin <mauloa32153@gmail.com>
2026-07-29 12:09:47 +0900

    Add app/robots.js

---
+++ app/robots.js
@@ -0,0 +1,7 @@
+export default function robots() {
+  return {
+    rules: { userAgent: "*", allow: "/" },
+    sitemap: "https://taishu-sakaba-hinode.vercel.app/sitemap.xml",
+    host: "https://taishu-sakaba-hinode.vercel.app"
+  };
+}
