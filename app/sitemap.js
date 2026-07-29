2f2d37ff7e74550ff7c2fe40281ce008324bfaa7
mauloajin <mauloa32153@gmail.com>
2026-07-29 12:09:48 +0900

    Add app/sitemap.js

---
+++ app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://taishu-sakaba-hinode.vercel.app/",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1
+    }
+  ];
+}
